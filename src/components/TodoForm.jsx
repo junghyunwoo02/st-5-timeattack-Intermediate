46dedb3de2b23ef626281002872156c496ec2d07
junghyunwoo02 <jungilbung@naver.com>
2024-06-10 16:37:34 +0900

    todolist 완료

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../redux/slices/todoSlice";
+
 export default function TodoForm() {
+  const todosTitle = useSelector((state) => state.todo);
+  const todosContent = useSelector((state) => state.todo);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!todosTitle || !todosContent) {
+      alert("제목과 내용을 모두 입력해주세요.");
+    }
+  };
   return (
-    <form>
-      <label>제목: </label>
-      <input type="text" /> <label>내용: </label>
-      <input type="text" />
-      <button>추가</button>
+    <form onSubmit={handleSubmit}>
+      <label>제목: {todosTitle} </label>
+      <input type="text" value={} onChange={}/> <label>내용: {todosContent}</label>
+      <input type="text" value={} onChange={}/>
+      <button onClick={() => dispatch(addTodo())}>추가</button>
     </form>
   );
 }
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,32 @@
 // TODO: todoSlice 를 작성하세요.
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: [],
+
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({
+        id: Date.now(),
+        title: action.payload,
+        content: action.payload,
+        isDone: false,
+      });
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
